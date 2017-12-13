fe03f1140ed2944bd04bb3bf76e3c633b910b337
Enrico <development@langhe.net>
2017-12-13 12:14:22 +0100

    read me

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var config      = require('./config.json');
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var sass        = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var sourceMaps  = require('gulp-sourcemaps');
+var rename      = require('gulp-rename');
+var postcss      = require('gulp-postcss');
+var autoprefixer      = require('autoprefixer');
+
+
+
+// browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    //watch files
+    var files = [
+    './style.css',
+    './*.php'
+    ];
+
+    //initialize browsersync
+    browserSync.init(files, {
+    //browsersync with a php server
+    proxy: config.proxy,
+    notify: false
+    });
+});
+
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task('sass', function () {
+    return gulp.src('sass/*.scss')
+        .pipe(sass({precision: '20'})) //compila sass
+        .pipe(postcss([ autoprefixer({ browsers: ['last 7 versions'] }) ])) // aggiunge css di autoprefixer
+        .pipe(rename('unmin-style.css')) //rinomina il file
+        .pipe(gulp.dest('./'))
+        .pipe(sourceMaps.init())
+        .pipe(cleanCSS({keepSpecialComments:1},{processImport: false}))
+        .pipe(rename('style.css')) //rinomina il file minifcato
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest('./'))
+        .pipe(reload({stream:true}));
+
+});
+
+// Default task to be run with `gulp`
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch("sass/**/*.scss", ['sass']);
+    gulp.watch("**/*.php", ['sass']);
+});
